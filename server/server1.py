0f14a7800733d839b7f9d020008cf5bea8ad17f2
AASmirnova <aasmirnova@mai.ru>
2021-12-28 17:49:11 +0300

    Work with k8

--- server/server1.py
+++ server/server1.py
@@ -13,21 +13,21 @@ class Server:
         self.register_api()
 
     def register_api(self):
-        self.sanic_app.add_route(PassDataCsv.as_view(), "/data")
-        self.sanic_app.add_route(GraphDeath.as_view(), "/graph_death")
-        self.sanic_app.add_route(GraphVac.as_view(), "/graph_vac")
+        self.sanic_app.add_route(PassDataCsv.as_view(), "/events/covid/data")
+        self.sanic_app.add_route(GraphDeath.as_view(), "/events/covid/graph_death")
+        self.sanic_app.add_route(GraphVac.as_view(), "/events/covid/graph_vac")
 
-        self.sanic_app.add_route(ProcessDataPercent.as_view(), "/percent")
-        self.sanic_app.add_route(DeleteData.as_view(), "/delete")
-        self.sanic_app.add_route(ContinentsList.as_view(), "/continents")
+        self.sanic_app.add_route(ProcessDataPercent.as_view(), "/events/covid/percent")
+        self.sanic_app.add_route(DeleteData.as_view(), "/events/covid/delete")
+        self.sanic_app.add_route(ContinentsList.as_view(), "/events/covid/continents")
 
         #Ann
-        self.sanic_app.add_route(ProcessDataCorrel.as_view(), "/correlation")
-        self.sanic_app.add_route(ProcessDataTopDeath.as_view(), "/top_death")
+        self.sanic_app.add_route(ProcessDataCorrel.as_view(), "/events/covid/correlation")
+        self.sanic_app.add_route(ProcessDataTopDeath.as_view(), "/events/covid/top_death")
         #Kirill
-        self.sanic_app.add_route(GraphDeathPercent.as_view(), "/graph_death_percent")
-        self.sanic_app.add_route(ProcessDataTopVac.as_view(), "/top_vac")
-        self.sanic_app.add_route(ContriesList.as_view(), "/countries")
+        self.sanic_app.add_route(GraphDeathPercent.as_view(), "/events/covid/graph_death_percent")
+        self.sanic_app.add_route(ProcessDataTopVac.as_view(), "/events/covid/top_vac")
+        self.sanic_app.add_route(ContriesList.as_view(), "/events/covid/countries")
 
     def run(self):
         self.sanic_app.run("0.0.0.0")
